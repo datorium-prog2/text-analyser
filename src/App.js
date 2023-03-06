246e4b4f435485d24572f0a62f666021b8ab9939
Serzsh <serzants.janis@gmail.com>
2023-03-06 14:25:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <h1 className='mt-3 mb-3'>Text analyser!</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Group className="mb-3" controlId="textToAnalyse">
+            <Form.Label>Text to analyse</Form.Label>
+            <Form.Control 
+              as="textarea" 
+              rows={10}
+              placeholder="Lorem ipsum dolor sit atmet..."
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row className='mb-5'>
+        <Col className="text-end">
+          <Button variant="info">
+            Analyse text
+          </Button>
+        </Col>
+      </Row>
+      <Row className='mb-3 pt-5'>
+        <Col xs={4}>
+          <h3>Top 5 used words</h3>
+          <ol>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+          </ol>
+        </Col>
+        <Col xs={4}>
+          <h3>Top 5 least used words</h3>
+          <ol>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+          </ol>
+        </Col>
+        <Col xs={4}>
+          <h3>Longest used word</h3>
+          <span>kautkadgarshvaards</span>
+        </Col>
+      </Row>
+      <Row className='mb-3'>
+        <Col xs={4}>
+          <h3>Top 5 used letters</h3>
+          <ol>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+          </ol>
+        </Col>
+        <Col xs={4}>
+          <h3>Top 5 least used letters</h3>
+          <ol>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+            <li>word</li>
+          </ol>
+        </Col>
+        <Col xs={4}>
+          <h3>shortest used word</h3>
+          <span>if</span>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+// top 5 used words
+// top 5 least used words
+// top 5 used letters
+// top 5 least used letters
+// longest word/-s
+// shortest word/-s
+
+export default App;
